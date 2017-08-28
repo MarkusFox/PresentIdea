da0c94eb87287a403ea59a64523a89062c3760bd
Markus Fox <markus@2a02-8388-c001-1880-089f-beb4-79b5-6b74.cable.dynamic.v6.surfer.at>
2017-08-28 11:30:14 +0200

    Added CoreData Support

--- PresentIdea/PresentsTableViewController.swift
+++ PresentIdea/PresentsTableViewController.swift
@@ -7,16 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
-class PresentsTableViewController: UITableViewController {
+class PresentsTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var myGifts = [["name":"Best Friend", "image":"1", "item":"Camera"],["name":"Mom", "image":"2", "item":"Flowers"],["name":"Dad", "image":"3", "item":"Some kind of tech"],["name":"Sister", "image":"4", "item":"Sweets"]]
+    var presents = [Present]()
+    
+    var managedObjectContext: NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let iconImageView = UIImageView(image: UIImage(named: "Shape"))
         self.navigationItem.titleView = iconImageView
+        
+        managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        loadData()
+    }
+    
+    func loadData() {
+        let presentRequest: NSFetchRequest<Present> = Present.fetchRequest()
+        
+        do {
+            presents = try managedObjectContext.fetch(presentRequest)
+            self.tableView.reloadData()
+        } catch {
+            print("Could not save data \(error.localizedDescription)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,66 +55,80 @@ class PresentsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return myGifts.count
+        return presents.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PresentsTableViewCell
-
-        let presentObject = myGifts[indexPath.row]
         
-        cell.nameLabel.text = presentObject["name"]
-        cell.itemLabel.text = presentObject["item"]
-        cell.backgroundImageView.image = UIImage(named: presentObject["image"]!)
-
+        let presentItem = presents[indexPath.row]
+        
+        if let presentImage = UIImage(data: presentItem.image! as Data) {
+            cell.backgroundImageView.image = presentImage
+        }
+        cell.nameLabel.text = presentItem.person
+        cell.itemLabel.text = presentItem.presentName
+        
         return cell
     }
+
+    @IBAction func addPresent(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        
+        self.present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            picker.dismiss(animated: true, completion: {
+                self.createPresentItem(with: image)
+            })
+        }
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func createPresentItem (with image: UIImage) {
+        let presentItem = Present(context: managedObjectContext)
+        presentItem.image = NSData(data: UIImageJPEGRepresentation(image, 0.3)!)
+        
+        //Standard Input Alert
+        let inputAlert = UIAlertController(title: "New Present", message: "Enter a person and a present", preferredStyle: .alert)
+        inputAlert.addTextField { (textfield: UITextField) in
+            textfield.placeholder = "Person"
+        }
+        inputAlert.addTextField { (textfield: UITextField) in
+            textfield.placeholder = "Present"
+        }
+        
+        inputAlert.addAction(UIAlertAction(title: "Save", style: .default, handler: { (action:UIAlertAction) in
+            let personTextField = inputAlert.textFields?.first
+            let presentTextField = inputAlert.textFields?.last
+            
+            if personTextField?.text != "" && presentTextField?.text != "" {
+                presentItem.person = personTextField?.text
+                presentItem.presentName = presentTextField?.text
+                
+                do {
+                    try self.managedObjectContext.save()
+                    self.loadData()
+                } catch {
+                    print("Could not save data \(error.localizedDescription)")
+                }
+            }
+        }))
+        
+        inputAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(inputAlert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
