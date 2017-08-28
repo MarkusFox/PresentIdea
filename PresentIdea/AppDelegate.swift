eb2d9c8d0a0b83667e98c53bbf235e92f82233c7
Markus Fox <markus@2a02-8388-c001-1880-089f-beb4-79b5-6b74.cable.dynamic.v6.surfer.at>
2017-08-28 09:52:28 +0200

    First Appearance, Status Bar lightColored

--- PresentIdea/AppDelegate.swift
+++ PresentIdea/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //StatusBar Text Color
+        UIApplication.shared.statusBarStyle = .lightContent
+        
+        
         return true
     }
 
