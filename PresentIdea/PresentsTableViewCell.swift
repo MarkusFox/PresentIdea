7e703afdf9e7d1e72c825c1d681ab637d32031ce
Markus Fox <markus@2a02-8388-c001-1880-089f-beb4-79b5-6b74.cable.dynamic.v6.surfer.at>
2017-08-28 10:26:37 +0200

    Added Design for Prototype Cells

---
+++ PresentIdea/PresentsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PresentsTableViewCell.swift
+//  PresentIdea
+//
+//  Created by Markus Fox on 28.08.17.
+//  Copyright © 2017 Markus Fox. All rights reserved.
+//
+
+import UIKit
+
+class PresentsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PresentIdea/PresentsTableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  PresentsTableViewController.swift
+//  PresentIdea
+//
+//  Created by Markus Fox on 28.08.17.
+//  Copyright © 2017 Markus Fox. All rights reserved.
+//
+
+import UIKit
+
+class PresentsTableViewController: UITableViewController {
+    
+    var myGifts = [["name":"Best Friend", "image":"1", "item":"Camera"],["name":"Mom", "image":"2", "item":"Flowers"],["name":"Dad", "image":"3", "item":"Some kind of tech"],["name":"Sister", "image":"4", "item":"Sweets"]]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let iconImageView = UIImageView(image: UIImage(named: "Shape"))
+        self.navigationItem.titleView = iconImageView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return myGifts.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PresentsTableViewCell
+
+        let presentObject = myGifts[indexPath.row]
+        
+        cell.nameLabel.text = presentObject["name"]
+        cell.itemLabel.text = presentObject["item"]
+        cell.backgroundImageView.image = UIImage(named: presentObject["image"]!)
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
